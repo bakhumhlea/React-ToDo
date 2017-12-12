9f4d2a48b9903251ac8f44d24b1c557a751affbc
Bakhumhlea <Tatuu@bakhumhlea.com>
2017-12-11 16:22:34 -0800

    checkpoint-7

--- bloc/react-to-do/src/App.js
+++ bloc/react-to-do/src/App.js
@@ -3,13 +3,23 @@ import './App.css';
 import ToDo from './components/ToDo.js';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos : [
+        {description:"Watch Star Wars", isCompleted: true},
+        {description:"Go have Pizza!", isCompleted: false},
+        {description:"Hang Out with Friend", isCompleted: false}
+      ]
+    };
+  }
   render() {
     return (
       <div className="App">
         <ul>
-          <ToDo />
-          <ToDo />
-          <ToDo />
+          { this.state.todos.map( (todo,index) =>
+            <ToDo key={index} description={todo.description} isCompleted={todo.isCompleted}/>
+          )}
         </ul>
       </div>
     );
